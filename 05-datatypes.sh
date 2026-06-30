c30941b5721d263813c62ffc019792473c1765b7
akhila konda <akhilakonda10@gmail.com>
2026-06-30 10:46:00 +0530

    data

--- 05-datatypes.sh
+++ 05-datatypes.sh
@@ -5,3 +5,11 @@ Num2=200
 
 sum=$((Num1 + Num2))
 echo "sum is: $sum"
+
+#Array
+
+Fruits=('Apple' 'Banana' 'Mango' 'Grapes')
+echo "Fruits are: ${Fruits[@]}"
+echo "First fruit is: ${Fruits[0]}"
+echo "second fruit is: $Fruits[1]}"
+echo "fourth fruit is: ${Fruits[3]}"
